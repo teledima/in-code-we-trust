27091f79b3bbc30fba3471ebcebd8f0bfdf403a0
teledima <teledima00@gmail.com>
2025-04-11 14:19:15 +0500

    Database migration Use definition of models at the time of the migration for seed_data migration

--- alembic/versions/dd911b2229f2_seed_data.py
+++ alembic/versions/dd911b2229f2_seed_data.py
@@ -9,11 +9,37 @@ import random
 from datetime import datetime, timedelta
 from typing import Sequence, Union
 
-import sqlalchemy as sa
-from sqlalchemy.orm import Session
+from sqlalchemy import DateTime, ForeignKey, String
+from sqlalchemy.orm import Mapped, Session, declarative_base, mapped_column
 
 from alembic import op
-from app.models import Category, Product, Sales
+
+Base = declarative_base()
+
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    category_id: Mapped[int] = mapped_column(ForeignKey("categories.id"), nullable=False)
+
+
+class Sales(Base):
+    __tablename__ = "sales"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    product_id: Mapped[int] = mapped_column(ForeignKey("products.id"), nullable=False)
+    amount: Mapped[int] = mapped_column(nullable=False)
+    dt: Mapped[datetime] = mapped_column(DateTime(), nullable=False)
+
 
 # revision identifiers, used by Alembic.
 revision: str = 'dd911b2229f2'
